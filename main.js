1b70c51345436cbf12d4f6d9e1de617caab8cd9f
Eric Polanco <eric.polanco@enemy.sh>
2025-01-24 16:56:25 -0500

    remove debug

--- main.js
+++ main.js
@@ -39,8 +39,6 @@ const verifyToken = (req, res, next) => {
         req.user = decoded
         next();
     } catch (err) {
-        console.log(err)
-        console.log(env.JWT_SECRET)
         return res.status(401).json({ message: "Invalid token"})
     }
 }
